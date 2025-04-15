9023bb4d48eb120c5eb51b3642096c8cb6455a1e
LionxxKing <nishanvr2003@gmail.com>
2025-04-15 23:39:00 +0530

    added project

--- ecommercedj/settings.py
+++ ecommercedj/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA = '/media/'
 # Default primary key field type
